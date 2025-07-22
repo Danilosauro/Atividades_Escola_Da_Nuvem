4552d2a59ab7b0b9498245d71f0a52c3fc18e4cb
danilosauro <danilodias_lc@hotmail.com>
2025-07-22 20:32:52 -0300

    Atividade 2

---
+++ ATIVIDADE_2/calculadora_descontos.py
@@ -0,0 +1,6 @@
+nome_produto = 'Camiseta' 
+preco_original = 50.00 
+desconto = 20 
+valor_final  = preco_original - (desconto *preco_original)/100 
+
+print(f'O produto {nome_produto}, com preço original R$ {preco_original}, tendo {desconto} % de desconto, sai por R$ {valor_final}')
---
+++ ATIVIDADE_2/calculadora_media.py
@@ -0,0 +1,10 @@
+nota_um  = input('Digite a nota 1: ')
+nota_dois =  input('Digite a nota 2: ')
+nota_tres =  input('Digite a nota 3: ') 
+
+nota_um = float(nota_um)
+nota_dois = float(nota_dois)
+nota_tres = float(nota_tres) 
+
+media = (nota_um + nota_dois + nota_tres)/3 
+print(f'A média é: {media:.2f}')
